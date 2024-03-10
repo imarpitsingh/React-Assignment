2dce55fe4f9fcdf0476fe6b0893b981c642cfde6
arpityadav <arpit.yadav@nagarro.com>
2024-03-10 15:51:49 +0530

    React App

---
+++ src/component/Basics/menuApi.js
@@ -0,0 +1,86 @@
+const Menu = [
+    {
+        id: 1,
+        image: "images/maggi.jpg",
+        name: "maggi",
+        category: "breakfast",
+        price: "12₹",
+        description:
+            "Maggi noodles are dried noodles fused with oil, and sold with a packet of flavorings. These noodles are usually eaten after being cooked in boiling water for 3 to 5 minutes or eaten straight from the packet. Just boil the water, add the tastemaker and noodles and your maggi is ready within 2 to 3 minutes.",
+    },
+
+    {
+        id: 2,
+        image: "images/allupakoida.jpg",
+        name: "allu pakoida",
+        category: "evening",
+        price: "20₹",
+        description:
+            "Aloo pakora are soft melt-in-the-mouth fried potato fritters coated in a delicious, golden, crispy gram flour batter. Make this awesome vegan potato snack also known as potato bajji in less than 30 minutes.",
+    },
+    {
+        id: 3,
+        image: "images/corn.jpg",
+        name: "corn",
+        category: "breakfast",
+        price: "10₹",
+        description:
+            "Bhutta is the Indian word for corn. Available in abundance during the monsoon, Bhutta is eaten either grilled or boiled but like most delicious Indian foods it is slathered in a bunch of spices (which is very different for the two versions) to help lift up the flavor profile to the quintessential Indian best!",
+    },
+    {
+        id: 4,
+        image: "../images/chola.jpg",
+        name: "chola",
+        category: "lunch",
+        price: "50₹",
+        description:
+            "Chole Masala is a popular Punjabi dish where chickpeas are simmered in a spicy & tangy gravy. “Chole” is a Punjabi word for “chickpeas” and “masala” is a word for “spices”. So this dish is nothing but chickpeas cooked to perfection with various spices, onions, tomatoes and herbs. Chickpea is a legume that is relished all over India.",
+    },
+    {
+        id: 5,
+        image: "../images/pizza.jpg",
+        name: "pizza",
+        category: "evening",
+        price: "80₹",
+        description:
+            "Pizza is a dish of Italian origin consisting of a usually round, flat base of leavened wheat-based dough topped with tomatoes, cheese, and often various other ingredients, which is then baked at a high temperature, traditionally in a wood-fired oven.",
+    },
+    {
+        id: 6,
+        image: "../images/nonvegthali.jpg",
+        name: "Non-Veg Thali",
+        category: "dinner",
+        price: "180₹",
+        description:
+            "Thali or Bhojanam is a round platter used to serve food in South Asia, Southeast Asia and the Caribbean. Thali is also used to refer to an Indian-style meal made up of a selection of various dishes which are served on a platter. Thali is also used in south Asia for ceremonial purposes.",
+    },
+    {
+        id: 7,
+        image: "../images/sweet.jpg",
+        name: "Sweets",
+        category: "dinner",
+        price: "60₹",
+        description:
+            "This is a list of Indian sweets and desserts, also called mithai, a significant element in Indian cuisine. Indians are known for their unique taste and experimental behavior when it comes to food. Many Indian desserts are fried foods made with sugar, milk or condensed milk. Ingredients and preferred types of dessert vary by region. In the eastern part of India, for example, most are based on milk products.",
+    },
+    {
+        id: 8,
+        image: "../images/rajmarice.jpg",
+        name: "Rajma Rice",
+        category: "lunch",
+        price: "60₹",
+        description:
+            "Rajma Rice. Rajma (Red kidney bean) is a protein rich bean curry cooked with onions and spices, and is best served with rice hence the name Rajma Rice. But this Rajma Masala can be enjoyed with parathas also.",
+    },
+    {
+        id: 9,
+        image: "../images/samosa.jpg",
+        name: "samaso",
+        category: "evening",
+        price: "10₹",
+        description:
+            "A samosa is a fried South Asian pastry with a savoury filling, including ingredients such as spiced potatoes, onions, peas, meat, or fish. It may take different forms, including triangular, cone, or half-moon shapes, depending on the region. Samosas are often accompanied by chutney.",
+    },
+];
+
+export default Menu;
